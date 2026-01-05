ba3f184dd15af2d2da043f924aa7f4a62c46154a
Gsus96 <jesus.reyes@desitegcom>
2026-01-04 21:52:24 -0600

    piedra papel o tijera finalizado

--- Control de flujo/piedra_papel_tijera.py
+++ Control de flujo/piedra_papel_tijera.py
@@ -1,6 +1,54 @@
-print("Bienvenido a piedra, papel o tijera")
+print("Bienvenido al juego")
+print('''Ingrese:
+    piedra
+    papel
+    tijera
+      ''')
+
 player1 = input("Ingresa tu opcion jugador 1: ")
-print(player1)
+if player1 == "piedra" or player1 == "papel" or player1 == "tijera":
+    print("La opcion del jugador 1 es valida")
+else:
+    print("La opcion del jugador 1 no es valida")
+    exit()
 
 player2 = input("Ingresa tu opcion jugador 2: ")
-print(player2)
+if player2 == "piedra" or player2 == "papel" or player2 == "tijera":
+    print("La opcion del jugador 2 es valida")
+else:
+    print("La opcion del jugador 2 no es valida")
+    exit()
+
+if player1 == "piedra" and player2 == "papel":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 2")
+
+elif player1 == "papel" and player2 == "piedra":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 1")
+
+elif player1 == "papel" and player2 == "tijera":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 2")
+
+elif player1 == "tijera" and player2 == "piedra":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 2")
+
+elif player1 == "tijera" and player2 == "papel":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 1")
+
+elif player1 == "piedra" and player2 == "tijera":
+    print("El jugador 1 eligió", player1, 
+          "El jugador 2 eligió", player2, 
+          "ganó el jugador 1")
+
+else:
+    print("Empate 🤝 ambos eligieron", player1)
+
